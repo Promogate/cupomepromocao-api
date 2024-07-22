b795c75830fcf35f078269d579bd790d654287a5
Augusto Dias <gutodiasdev@gmail.com>
2024-07-22 18:28:14 -0300

    chore(w/test): add express and axios

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,9 @@
-console.log("Testing!");
+import express from "express";
+
+const app = express();
+
+app.get("/test", (request, response) => {
+  return response.status(200).json({  message: "ok!" })
+});
+
+app.listen(8080, () => console.log("Working!"));
--- test/index.test.ts
+++
@@ -1,8 +0,0 @@
-describe("test", () => {
-  test("testing", () => {
-    
-    expect(1).toBe(1);
-  });
-});
-
-export {}
