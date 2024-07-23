cf987d3ab1b2d9cee31de2e5d5a5a217b2e951a2
Augusto Dias <gutodiasdev@gmail.com>
2024-07-23 15:03:59 -0300

    refact: add a single store in StoreDatabaseMemory

--- src/database/memory/StoreDatabaseMemory.ts
+++ src/database/memory/StoreDatabaseMemory.ts
@@ -3,8 +3,16 @@ import { IStoreDatabase } from "../contracts/Store";
 
 export class StoreDatabaseMemory implements IStoreDatabase {
   stores: Store[];
-  constructor () {
-    this.stores = []
+  constructor() {
+    this.stores = [
+      {
+        id: 1,
+        name: "any_name",
+        store_link: "https://google.com.br",
+        created_at: "any_date",
+        updated_at: "any_date"
+      }
+    ]
   }
 
   async findByName(name: string): Promise<Store> {
