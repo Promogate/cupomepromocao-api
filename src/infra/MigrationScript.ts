6c0df4cd6fbd03b6ff1975dc864ad318652fbdc6
Augusto Dias <gutodiasdev@gmail.com>
2024-07-23 20:16:19 -0300

    feat: add migration

--- src/infra/MigrationScript.ts
+++ src/infra/MigrationScript.ts
@@ -1,6 +1,6 @@
 const migrationScript = `
   CREATE TABLE IF NOT EXISTS stores (
-    id SERIAL PRIMARY KEY,
+    id VARCHAR(16) PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     store_link VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -8,8 +8,8 @@ const migrationScript = `
   );
 
   CREATE TABLE IF NOT EXISTS offers (
-    id SERIAL PRIMARY KEY,
-    store_id INTEGER NOT NULL,
+    id VARCHAR(16) PRIMARY KEY,
+    store_id VARCHAR(16) NOT NULL,
     offer_title VARCHAR(255) NOT NULL,
     offer_description TEXT,
     price DECIMAL(10, 2) NOT NULL,
@@ -18,6 +18,8 @@ const migrationScript = `
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (store_id) REFERENCES stores (id) ON DELETE CASCADE
   );
+
+  ALTER TABLE offers ADD COLUMN destination_link VARCHAR(255);
 `;
 
 export default migrationScript;
