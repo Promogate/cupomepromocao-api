acb87397c5284abd146e12d996eeeff8b087a9eb
Augusto Dias <gutodiasdev@gmail.com>
2024-07-23 15:07:06 -0300

    refact: change StoreDAO constructor interface to IStoreDatabase

--- src/database/dao/Store.ts
+++ src/database/dao/Store.ts
@@ -1,11 +1,12 @@
 import { Store } from "../../application/@types";
+import { IStoreDatabase } from "../contracts/Store";
 
 export interface IStoreDAO {
   findByName(name: string): Promise<Store>;
 }
 
 export class StoreDAO implements IStoreDAO {
-  constructor(readonly storeDAO: IStoreDAO) {}
+  constructor(readonly storeDAO: IStoreDatabase) {}
   
   async findByName(name: string): Promise<Store> {
     const store = await this.storeDAO.findByName(name);
