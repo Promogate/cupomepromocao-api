18096d1bd985fdcddb4440f40412c485a7d67508
Augusto Dias <gutodiasdev@gmail.com>
2024-07-23 14:13:53 -0300

    refact: move Store tests resources to their own files

--- test/Store.test.ts
+++ test/Store.test.ts
@@ -1,47 +1,42 @@
 import Sinon from "sinon";
+import { CreateStoreService } from "../src/application/services/CreateStoreService";
+import { StoreDatabaseMemory } from "../src/database/memory/StoreDatabaseMemory";
+import { StoreDAO } from "../src/database/dao/Store";
 
-interface ICreateStore {
-  execute(input: ICreateStore.Input): Promise<ICreateStore.Output>;
-}
-
-namespace ICreateStore {
-  export type Input = {
-    name: string;
-    storeLink: string;
-  };
-  export type Output = {
-    id: number;
-    name: string;
-    store_link: string;
-    created_at: string;
-    updated_at: string;
-  };
-}
-
-class CreateStoreService implements ICreateStore {
-  async execute(input: ICreateStore.Input): Promise<ICreateStore.Output> {
-    return {
-      id: 1,
-      name: input.name,
-      store_link: input.storeLink,
-      created_at: "any_date",
-      updated_at: "any_date"
-    }
-  }
-}
-
-describe("Create Store", () => {
-  it("it should call execute method with correct params", async () => {
+describe("Stores Suite", () => {
+  it("should call execute method with correct params", async () => {
     const input = {
       name: "any_name",
       storeLink: "any_link"
     }
     const sutSpy = Sinon.spy(CreateStoreService.prototype, "execute");
-    const sut = new CreateStoreService();
+    const database = new StoreDatabaseMemory()
+    const storeDAO = new StoreDAO(database);
+    const sut = new CreateStoreService(storeDAO);
     await sut.execute(input);
     expect(sutSpy.calledWith({
       name: "any_name",
       storeLink: "any_link"
     })).toBeTruthy();
+    expect(sutSpy.calledOnce).toBeTruthy();
+    sutSpy.restore();
+  });
+  it("should send an error if store name already exists", async () => {
+    const input = {
+      name: "any_name",
+      storeLink: "any_link"
+    }
+    const storeDAOStub = Sinon.stub(StoreDAO.prototype, "findByName").resolves({
+      id: 1,
+      name: "any_name",
+      store_link: "any_link",
+      created_at: "any_date",
+      updated_at: "any_date"
+    });
+    const database = new StoreDatabaseMemory()
+    const storeDAO = new StoreDAO(database);
+    const sut = new CreateStoreService(storeDAO);
+    await expect(() => sut.execute(input)).rejects.toThrow();
+    storeDAOStub.restore();
   });
 });
