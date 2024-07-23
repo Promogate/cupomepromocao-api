8c1cc9da0344f330e68dd19443076190ff22a4d6
Augusto Dias <gutodiasdev@gmail.com>
2024-07-23 14:13:16 -0300

    refact: change from 'test' to 'it' in tests

--- test/Express.test.ts
+++ test/Express.test.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 describe("Express", () => {
-  test("it should return 'ok!' on '/test' route", async () => {
+  it("should return 'ok!' on '/test' route", async () => {
     const sut = await axios.get("http://localhost:8080/test");
     expect(sut.status).toBe(200);
     expect(sut.data.message).toBe("ok!");
